b178f18a049192b7dac18542558c9ab2c9a2c63d
serhiy12345 <baksergij15@gmail.com>
2024-06-07 09:42:24 +0300

    Added all Thymeleaf expressions demonstrated in lecture 2

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -5,7 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MyController {
@@ -17,6 +19,16 @@ public class MyController {
 
         model.addAttribute("message", message);
         model.addAttribute("items", items);
+
+        // Additional attributes for Thymeleaf expressions
+        model.addAttribute("isVisible", true);
+        model.addAttribute("number", 123);
+        model.addAttribute("url", "https://www.example.com");
+        model.addAttribute("date", new java.util.Date());
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        model.addAttribute("map", map);
 
         return "demo";
     }
